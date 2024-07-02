0bbc9fc22761523f559b8d27cd5e6d13df5395f3
PS <pramilanarawade70@gmail.com>
2024-07-02 14:05:51 +0530

    Update and rename build-php.sh to build-java.sh

--- build-php.sh
+++
@@ -1,9 +0,0 @@
-cd /path/to/springboot1-application
-echo '#!/bin/bash
-set -e
-cd /var/lib/jenkins/workspace/Springboot-python\ application/springboot1-application
-./mvnw clean
-./mvnw compile
-./mvnw package
-echo "Build completed successfully."' > build-java.sh
-chmod +x build-java.sh
