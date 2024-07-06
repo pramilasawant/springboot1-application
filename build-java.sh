814ec43733da63e0cb73f89d9b82f4ff0650ff1d
PS <pramilanarawade70@gmail.com>
2024-07-06 15:32:27 +0530

    Update build-java.sh

--- build-java.sh
+++ build-java.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
 echo "Building Java application..."
-
-# Navigate to the project root directory where mvnw is located
-cd $(dirname "$0")
-
-# Clean and package the Spring Boot application using Maven
 ./mvnw clean package
 
-if [ $? -ne 0 ]; then
-  echo "Build failed"
-  exit 1
-fi
-
-echo "Build successful"
